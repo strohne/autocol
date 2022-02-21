a2bc81cfff7e3239d55bde0ab2ded75dd50cc24c
ChantalGrtnr <gaertner.chantal@gmail.com>
2022-02-21 19:33:00 +0100

    Add webscraping files

---
+++ 3_Webscraping/readme.md
@@ -0,0 +1,13 @@
+# Webscraping and Webcrawling
+The scripts contain examples and exercises for classical web scraping and for browser automated scraping with Selenium.
+
+# Exercises: Webscraping
+**Task 1**
+Open the webpage https://edition.cnn.com/search?q=Science. Find the element with the number of search results in the source code!
+
+**Task 2**
+Open one article that you get from the search results (for example https://edition.cnn.com/2022/02/19/tech/facebook-rebranding-problems/index.html). 
+What is the HTML element that contains the text of the article?
+
+**Task 3**
+With which CSS-Selector can you select the element with the text of the article? 
---
+++ 3_Webscraping/webscraping.py
@@ -0,0 +1,78 @@
+# Webscraping with Python
+# Extract articles from Al Jazeera
+
+# Install packages, if not used before
+# pip install os 
+# pip install requests 
+# pip install beautifulsoup4
+# pip install lxml
+# pip install pandas
+
+
+# Embed libraries
+import os
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# Set folder and filename for downloads
+# (create file if it doesn't exist yet)
+directory = "html"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
+filename = directory + "/articles.html"
+    
+    
+# Construct URL from path and searchterm
+path = "https://www.aljazeera.com/search/"
+searchterm = "Turkey"
+url = path + searchterm
+
+
+# Download webpage
+# Get status code
+response = requests.get(url)  
+# If status code = 200, download webpage
+if response.status_code == 200:
+    with open(filename, 'wb') as file:
+        file.write(response.content)   
+
+        
+# Parse webpage
+soup = BeautifulSoup(open(filename, encoding="utf-8"),'lxml')
+
+
+# Get all article-elements from soup
+articles = soup.select('article')
+
+
+# Scrape information from articles
+
+# Initialize empty list for results
+results = []
+
+for article in articles: 
+    
+    # Initialize emty dictionary
+    # Extract title and text of articles 
+    item = {}
+    item['title'] = article.select_one('span').text.strip()
+    item['text'] = article.select_one('p').text.strip()
+    
+    # Append items to result-list
+    results.append(item)
+    
+# Exercise: Scrape the URL of the article. 
+
+
+# Convert results-list to dataframe
+results = pd.DataFrame(results)
+
+# Display dataframe
+display(results)
+
+
+# Write dataframe as CSV-file
+results.to_csv('results.csv',sep=";",index=False)
