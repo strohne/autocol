373843c2085df8021e08dc458f854c366400628e
strohne <krimskram@freenet.de>
2022-02-22 08:43:42 +0100

    Add Facepager data python script

---
+++ 2_apis/readfacepagerdata.py
@@ -0,0 +1,58 @@
+#
+# Import packages ----
+#
+
+import pandas as pd
+import matplotlib.pyplot as plt  
+%matplotlib inline
+
+#
+# Load dataset ----
+#
+
+df = pd.read_csv("facebooknews.csv", sep=';')
+
+# Show data
+display(df)
+
+#
+# Select data ----
+#
+
+# Select rows
+df.loc[df.object_type == 'data',:]
+df.loc[(df.object_type == 'data') & (df.level == 1),:]
+
+
+# Select columns
+df.loc[:,['object_id','created_time','comments','reactions','shares','message','from.name']]
+
+# Select rows and columns
+df = df.loc[(df.object_type == 'data') & (df.level == 1),['object_id','created_time','comments','reactions','shares','message','from.name']]
+
+#
+# Describe data ----
+#
+
+
+# Count posts by author
+df.loc[:,'from.name'].value_counts()
+
+# Distribution of the metrics
+df.describe()
+
+# Order by comment count to get the post with the most comments
+df.sort_values(by=['comments'], ascending=False)
+
+#
+# Plots ----
+#
+
+# Histogram
+df.hist("reactions")
+
+# Scatter plot
+df.plot.scatter("shares","comments")
+
+# Log scale histogram
+plt.hist(df['comments'], log=True)
