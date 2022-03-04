f79fdb3c0950029c66a4b1c054cec8f84c0932a7
ChantalGrtnr <gaertner.chantal@gmail.com>
2022-03-04 08:35:14 +0100

    update regex

--- 3_webscraping/regularexpressions.py
+++ 3_webscraping/regularexpressions.py
@@ -10,18 +10,12 @@ re.findall(r"myfancyregex", text)
 
 # Load scraped data
 import pandas as pd
-articles = pd.read_csv("data/articles-search.csv", sep=";")
+articles = pd.read_csv("aljazeera-articles-search-facepager.csv", sep=";")
 articles
 
+
 # Extract the date in our scraped data
-
-articles['date']  = articles['url'].str.extract('([0-9]+/[0-9]+/[0-9]+)')
-
-# Convert to datetime object
-articles['date'] = pd.to_datetime(articles['date'], dayfirst=False)
-
-# Construct month string
-articles['month'] = articles['date'].dt.strftime('%Y-%m')
+articles['date']  = articles['link'].str.extract('([0-9]+/[0-9]+/[0-9]+)')
 
 # Drop rows with missing date
 articles = articles.dropna()
@@ -29,22 +23,17 @@ articles = articles.dropna()
 articles = articles.sort_values(by=['date'])
 
 
-# Extract search term
-
-articles['term'] = articles['filename'].str.extract('([^_]+$)')
-
-articles
-
-# Aggregate by month
-
-timeline= articles.groupby(['month','term']).size().to_frame('count')
+# Aggregate by day
+timeline= articles.groupby(['date','term']).size().to_frame('count')
 timeline= timeline.reset_index()
-timeline = timeline.sort_values(by=['month'])
+timeline = timeline.sort_values(by=['date'])
 timeline
 
 
 # Plot timeline
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-plot = sns.lineplot(x='month', y='count', hue='term', data=timeline)
+plt.figure(figsize=(15,5))
+plot = sns.lineplot(x='date', y='count', hue='term', data=timeline)
 plot.tick_params(axis='x', rotation=90)
